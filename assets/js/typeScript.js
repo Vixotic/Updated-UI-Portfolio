c6146cec48aac5d2eefb6b9d1ca1a0de7b2368be
Infamous-Vix <vixnightwalker@gmail.com>
2021-08-23 11:31:31 +0530

    Bulk added entire repository

---
+++ assets/js/typeScript.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.type', {
+    strings: ['Graphics Designer', 'Writer', 'Painter', 'Web Developer'],
+    smartBackspace: true,
+    loop: true,
+    showCursor: false,
+    typeSpeed: 40,
+    backSpeed: 40
+});
